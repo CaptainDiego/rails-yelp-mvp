71aac06d5efc96aebee81bddbfd3657b7ae6a57a
Diego Gilon <diego.gilon@gmail.com>
2018-03-14 16:43:18 +0100

    adding reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,20 +13,26 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number, :review)
   end
 
 end
